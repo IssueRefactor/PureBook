7419d1e98271c1117e073293f674c7ecc62861ba
yuhang-dong <dong_yu_hang@126.com>
2022-10-05 20:10:11 +0800

    fix: uri

--- src/utils/parser/render/mdRender.ts
+++ src/utils/parser/render/mdRender.ts
@@ -1,7 +1,7 @@
 import { renderMarkdown } from "@astrojs/markdown-remark";
 import { navigate } from "@utils/application/navigate";
 import * as shiki from 'shiki';
-shiki.setCDN(navigate.to('/assets/shiki/'))
+shiki.setCDN(navigate.to('assets/shiki/'))
 export const renderMd = (content: string) => {
   return renderMarkdown(
     content,
