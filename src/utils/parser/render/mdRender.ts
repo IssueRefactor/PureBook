a2975f405575c329c1f84662e28f8fbe6b1e93f7
yuhang-dong <dong_yu_hang@126.com>
2022-10-05 19:44:43 +0800

    fix: shiki CDN

--- src/utils/parser/render/mdRender.ts
+++ src/utils/parser/render/mdRender.ts
@@ -1,6 +1,6 @@
 import { renderMarkdown } from "@astrojs/markdown-remark";
 import * as shiki from 'shiki';
-shiki.setCDN('/node_modules/shiki/')
+shiki.setCDN('https://cdn.jsdelivr.net/npm/shiki@0.11.1/')
 export const renderMd = (content: string) => {
   return renderMarkdown(
     content,
